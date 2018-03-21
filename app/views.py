1ba73c7af62b0e4acc96e88cc4bd18c4fbcfceba
SuperWong <wangchao_198912@126.com>
2018-03-21 08:03:28 +0800

    templates test

--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, world!'
+    user = {'nickname':'张三'}
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html", title='Home', user=user, posts=posts)
