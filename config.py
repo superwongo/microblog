dc181dc0f176260748dba04888efbc27a52a9e27
SuperWong <wangchao_198912@126.com>
2018-03-22 23:37:24 +0800

    db file

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# CSRF_ENABLED 配置是为了激活 跨站点请求伪造 保护
+CSRF_ENABLED = True
+# ECRET_KEY 配置仅仅当 CSRF 激活的时候才需要，它是用来建立一个加密的令牌，用于验证一个表单。
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
+]
+
+# Flask-SQLAlchemy 扩展需要的数据库文件的路径
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# SQLAlchemy-migrate 数据文件存储目录
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
